99824c28ec4ea81d54228e033d64cf72236e74ab
Thomas Thron <throni3git@gmail.com>
2018-08-07 21:18:38 +0200

    refactor stuff into utils.ts

--- src/LightSetup.ts
+++ src/LightSetup.ts
@@ -1,90 +1,5 @@
 import * as THREE from "three";
-
-function polarToCartRad(
-  azimuthRad: number,
-  elevationRad: number,
-  radius: number = 1
-): THREE.Vector3 {
-  const result = {
-    x: Math.sin(azimuthRad) * Math.cos(elevationRad) * radius,
-    y: Math.sin(elevationRad) * radius,
-    z: Math.cos(azimuthRad) * Math.cos(elevationRad) * radius
-  } as THREE.Vector3;
-
-  return result;
-}
-
-function polarToCartDeg(
-  azimuthDeg: number,
-  elevationDeg: number,
-  radius: number
-): THREE.Vector3 {
-  return polarToCartRad(
-    (azimuthDeg / 180) * Math.PI,
-    (elevationDeg / 180) * Math.PI,
-    radius
-  );
-}
-
-function cartToPolarRad(
-  direction: THREE.Vector3
-): { azimuth: number; elevation: number; radius: number } {
-  const length = Math.sqrt(
-    direction.x * direction.x +
-      direction.y * direction.y +
-      direction.z * direction.z
-  );
-  if (length === 0) {
-    return {
-      azimuth: 0,
-      elevation: 0,
-      radius: 0
-    };
-  }
-  const normDir = {
-    x: direction.x / length,
-    y: direction.y / length,
-    z: direction.z / length
-  };
-  const xzLength =
-    Math.sqrt(normDir.x * normDir.x + normDir.z * normDir.z) + 1e-8;
-  const elevation = Math.asin(normDir.y);
-  const azimuth = Math.atan2(normDir.x, normDir.z);
-
-  return {
-    azimuth: azimuth,
-    elevation: elevation,
-    radius: length
-  };
-}
-
-function cartToPolarDeg(
-  direction: THREE.Vector3
-): { azimuth: number; elevation: number; radius: number } {
-  const resultRad = cartToPolarRad(direction);
-  const resultDeg = {
-    azimuth: (resultRad.azimuth * 180) / Math.PI,
-    elevation: (resultRad.elevation * 180) / Math.PI,
-    radius: resultRad.radius
-  };
-  return resultDeg;
-}
-
-async function getLightDescription(url: string): Promise<ILightSetup> {
-  const promise = new Promise<ILightSetup>((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = "json";
-    xhr.onreadystatechange = (bla: Event) => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        const parsed = xhr.response as ILightSetup;
-        resolve(parsed);
-      }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-  });
-  return promise;
-}
+import { loadJsonFile, polarToCartDeg, polarToCartRad } from "./utils";
 
 const DEBUG = true;
 
@@ -143,7 +58,7 @@ export async function loadAndReplaceLightSetup(
   options?: ILightSetupOptions
 ): Promise<void> {
   try {
-    const lightSetup = await getLightDescription(url);
+    const lightSetup = await loadJsonFile<ILightSetup>(url);
     replaceLightSetup(lightSetup, scene, renderer, options);
   } catch (e) {
     console.warn("LightSetup: file " + url + " could not be loaded.");
--- src/utils.ts
+++ src/utils.ts
@@ -25,3 +25,75 @@ export async function loadJsonFile<T>(url: string): Promise<T> {
   });
   return promise;
 }
+
+
+
+export function polarToCartRad(
+  azimuthRad: number,
+  elevationRad: number,
+  radius: number = 1
+): THREE.Vector3 {
+  const result = {
+    x: Math.sin(azimuthRad) * Math.cos(elevationRad) * radius,
+    y: Math.sin(elevationRad) * radius,
+    z: Math.cos(azimuthRad) * Math.cos(elevationRad) * radius
+  } as THREE.Vector3;
+
+  return result;
+}
+
+export function polarToCartDeg(
+  azimuthDeg: number,
+  elevationDeg: number,
+  radius: number
+): THREE.Vector3 {
+  return polarToCartRad(
+    (azimuthDeg / 180) * Math.PI,
+    (elevationDeg / 180) * Math.PI,
+    radius
+  );
+}
+
+export function cartToPolarRad(
+  direction: THREE.Vector3
+): { azimuth: number; elevation: number; radius: number } {
+  const length = Math.sqrt(
+    direction.x * direction.x +
+      direction.y * direction.y +
+      direction.z * direction.z
+  );
+  if (length === 0) {
+    return {
+      azimuth: 0,
+      elevation: 0,
+      radius: 0
+    };
+  }
+  const normDir = {
+    x: direction.x / length,
+    y: direction.y / length,
+    z: direction.z / length
+  };
+  const xzLength =
+    Math.sqrt(normDir.x * normDir.x + normDir.z * normDir.z) + 1e-8;
+  const elevation = Math.asin(normDir.y);
+  const azimuth = Math.atan2(normDir.x, normDir.z);
+
+  return {
+    azimuth: azimuth,
+    elevation: elevation,
+    radius: length
+  };
+}
+
+export function cartToPolarDeg(
+  direction: THREE.Vector3
+): { azimuth: number; elevation: number; radius: number } {
+  const resultRad = cartToPolarRad(direction);
+  const resultDeg = {
+    azimuth: (resultRad.azimuth * 180) / Math.PI,
+    elevation: (resultRad.elevation * 180) / Math.PI,
+    radius: resultRad.radius
+  };
+  return resultDeg;
+}
