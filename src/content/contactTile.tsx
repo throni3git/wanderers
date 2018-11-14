4a5d54b005fe04941dec71d727426aac2a7c9989
Thomas Thron <throni3git@gmail.com>
2018-11-14 13:55:08 +0100

    begin implementing contact tile

--- src/content/contactTile.tsx
+++ src/content/contactTile.tsx
@@ -13,196 +13,35 @@ import {
 export class ContactTile extends React.Component<
   IContactTileProps,
   IContactTileState
-> {
+  > {
   constructor(props: ContactTile["props"]) {
     super(props);
+    this.state = { name: '', mail: '', text: '', acceptsDSGVO: false }
+
+  }
+
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const changedValue = event.target.value;
+    const changedEntry = event.target.name;
+    this.setState({ [changedEntry]: changedValue });
+  }
+
+  private onSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
   }
 
   public render() {
     return (
       <ScrollComponent>
         <UnitEntryContainer>
-          <UnitEntryCaption>
-            <UnitEntryCaptionText>Contact</UnitEntryCaptionText>
-          </UnitEntryCaption>
-          <UnitEntryContent>
-            <p>
-              Fugiat sunt nisi sunt velit ex excepteur do reprehenderit
-              excepteur culpa esse veniam proident anim. Sint commodo nisi qui
-              nisi consequat eu. Consequat laboris labore mollit aliquip.
-            </p>
-            <p>
-              Adipisicing qui est minim veniam sunt quis occaecat ex qui anim.
-              Sit esse esse dolor proident fugiat mollit id dolore. Officia
-              reprehenderit quis ad exercitation proident ea est irure aliquip.
-            </p>
-            <p>
-              Quis ex aute officia sint magna nisi nulla et laboris incididunt
-              pariatur pariatur nulla commodo. Nulla adipisicing dolore
-              cupidatat culpa laboris aliquip consequat incididunt esse eiusmod
-              aliqua aliquip tempor. Est officia excepteur aliqua nulla ut
-              aliquip consequat deserunt fugiat. Pariatur sint veniam eu
-              pariatur in incididunt fugiat in sit ad aliqua cupidatat
-              consectetur.
-            </p>
-            <p>
-              Voluptate sunt Lorem consectetur enim proident ut irure est veniam
-              est nostrud consequat ullamco dolor. Cillum laborum sit culpa nisi
-              in duis mollit. Mollit occaecat aliqua commodo ipsum aute commodo
-              quis. Reprehenderit deserunt labore mollit tempor do tempor fugiat
-              dolor qui. Ullamco esse veniam culpa magna esse pariatur officia
-              nostrud enim proident eu sit. Consectetur quis id in aute
-              excepteur ullamco irure aliqua nulla. Sit ipsum elit ut in eiusmod
-              amet ullamco enim sunt ex tempor reprehenderit cupidatat.
-            </p>
-            <p>
-              Adipisicing voluptate do et ullamco consequat enim anim id tempor
-              duis reprehenderit enim dolore irure. Labore aliqua nulla cillum
-              sit excepteur in. Ipsum cillum proident ipsum commodo duis ad qui.
-              Cupidatat veniam ipsum nisi proident incididunt. Voluptate tempor
-              ea eu incididunt proident laborum ad commodo labore duis cillum
-              eiusmod qui aliqua.
-            </p>
-            <p>
-              Et anim velit sint sit laboris irure nulla ex ullamco dolore.
-              Exercitation elit esse esse magna. Labore qui cillum quis sit non
-              quis voluptate nisi. Cillum nulla elit voluptate voluptate. Sint
-              sunt quis cupidatat ad excepteur eu Lorem do exercitation. Et
-              dolor sint labore amet culpa laboris amet cupidatat excepteur.
-            </p>
-            <p>
-              Fugiat sunt nisi sunt velit ex excepteur do reprehenderit
-              excepteur culpa esse veniam proident anim. Sint commodo nisi qui
-              nisi consequat eu. Consequat laboris labore mollit aliquip.
-            </p>
-            <p>
-              Adipisicing qui est minim veniam sunt quis occaecat ex qui anim.
-              Sit esse esse dolor proident fugiat mollit id dolore. Officia
-              reprehenderit quis ad exercitation proident ea est irure aliquip.
-            </p>
-            <p>
-              Quis ex aute officia sint magna nisi nulla et laboris incididunt
-              pariatur pariatur nulla commodo. Nulla adipisicing dolore
-              cupidatat culpa laboris aliquip consequat incididunt esse eiusmod
-              aliqua aliquip tempor. Est officia excepteur aliqua nulla ut
-              aliquip consequat deserunt fugiat. Pariatur sint veniam eu
-              pariatur in incididunt fugiat in sit ad aliqua cupidatat
-              consectetur.
-            </p>
-            <p>
-              Voluptate sunt Lorem consectetur enim proident ut irure est veniam
-              est nostrud consequat ullamco dolor. Cillum laborum sit culpa nisi
-              in duis mollit. Mollit occaecat aliqua commodo ipsum aute commodo
-              quis. Reprehenderit deserunt labore mollit tempor do tempor fugiat
-              dolor qui. Ullamco esse veniam culpa magna esse pariatur officia
-              nostrud enim proident eu sit. Consectetur quis id in aute
-              excepteur ullamco irure aliqua nulla. Sit ipsum elit ut in eiusmod
-              amet ullamco enim sunt ex tempor reprehenderit cupidatat.
-            </p>
-            <p>
-              Adipisicing voluptate do et ullamco consequat enim anim id tempor
-              duis reprehenderit enim dolore irure. Labore aliqua nulla cillum
-              sit excepteur in. Ipsum cillum proident ipsum commodo duis ad qui.
-              Cupidatat veniam ipsum nisi proident incididunt. Voluptate tempor
-              ea eu incididunt proident laborum ad commodo labore duis cillum
-              eiusmod qui aliqua.
-            </p>
-            <p>
-              Et anim velit sint sit laboris irure nulla ex ullamco dolore.
-              Exercitation elit esse esse magna. Labore qui cillum quis sit non
-              quis voluptate nisi. Cillum nulla elit voluptate voluptate. Sint
-              sunt quis cupidatat ad excepteur eu Lorem do exercitation. Et
-              dolor sint labore amet culpa laboris amet cupidatat excepteur.
-            </p>
-            <p>
-              Fugiat sunt nisi sunt velit ex excepteur do reprehenderit
-              excepteur culpa esse veniam proident anim. Sint commodo nisi qui
-              nisi consequat eu. Consequat laboris labore mollit aliquip.
-            </p>
-            <p>
-              Adipisicing qui est minim veniam sunt quis occaecat ex qui anim.
-              Sit esse esse dolor proident fugiat mollit id dolore. Officia
-              reprehenderit quis ad exercitation proident ea est irure aliquip.
-            </p>
-            <p>
-              Quis ex aute officia sint magna nisi nulla et laboris incididunt
-              pariatur pariatur nulla commodo. Nulla adipisicing dolore
-              cupidatat culpa laboris aliquip consequat incididunt esse eiusmod
-              aliqua aliquip tempor. Est officia excepteur aliqua nulla ut
-              aliquip consequat deserunt fugiat. Pariatur sint veniam eu
-              pariatur in incididunt fugiat in sit ad aliqua cupidatat
-              consectetur.
-            </p>
-            <p>
-              Voluptate sunt Lorem consectetur enim proident ut irure est veniam
-              est nostrud consequat ullamco dolor. Cillum laborum sit culpa nisi
-              in duis mollit. Mollit occaecat aliqua commodo ipsum aute commodo
-              quis. Reprehenderit deserunt labore mollit tempor do tempor fugiat
-              dolor qui. Ullamco esse veniam culpa magna esse pariatur officia
-              nostrud enim proident eu sit. Consectetur quis id in aute
-              excepteur ullamco irure aliqua nulla. Sit ipsum elit ut in eiusmod
-              amet ullamco enim sunt ex tempor reprehenderit cupidatat.
-            </p>
-            <p>
-              Adipisicing voluptate do et ullamco consequat enim anim id tempor
-              duis reprehenderit enim dolore irure. Labore aliqua nulla cillum
-              sit excepteur in. Ipsum cillum proident ipsum commodo duis ad qui.
-              Cupidatat veniam ipsum nisi proident incididunt. Voluptate tempor
-              ea eu incididunt proident laborum ad commodo labore duis cillum
-              eiusmod qui aliqua.
-            </p>
-            <p>
-              Et anim velit sint sit laboris irure nulla ex ullamco dolore.
-              Exercitation elit esse esse magna. Labore qui cillum quis sit non
-              quis voluptate nisi. Cillum nulla elit voluptate voluptate. Sint
-              sunt quis cupidatat ad excepteur eu Lorem do exercitation. Et
-              dolor sint labore amet culpa laboris amet cupidatat excepteur.
-            </p>
-            <p>
-              Fugiat sunt nisi sunt velit ex excepteur do reprehenderit
-              excepteur culpa esse veniam proident anim. Sint commodo nisi qui
-              nisi consequat eu. Consequat laboris labore mollit aliquip.
-            </p>
-            <p>
-              Adipisicing qui est minim veniam sunt quis occaecat ex qui anim.
-              Sit esse esse dolor proident fugiat mollit id dolore. Officia
-              reprehenderit quis ad exercitation proident ea est irure aliquip.
-            </p>
-            <p>
-              Quis ex aute officia sint magna nisi nulla et laboris incididunt
-              pariatur pariatur nulla commodo. Nulla adipisicing dolore
-              cupidatat culpa laboris aliquip consequat incididunt esse eiusmod
-              aliqua aliquip tempor. Est officia excepteur aliqua nulla ut
-              aliquip consequat deserunt fugiat. Pariatur sint veniam eu
-              pariatur in incididunt fugiat in sit ad aliqua cupidatat
-              consectetur.
-            </p>
-            <p>
-              Voluptate sunt Lorem consectetur enim proident ut irure est veniam
-              est nostrud consequat ullamco dolor. Cillum laborum sit culpa nisi
-              in duis mollit. Mollit occaecat aliqua commodo ipsum aute commodo
-              quis. Reprehenderit deserunt labore mollit tempor do tempor fugiat
-              dolor qui. Ullamco esse veniam culpa magna esse pariatur officia
-              nostrud enim proident eu sit. Consectetur quis id in aute
-              excepteur ullamco irure aliqua nulla. Sit ipsum elit ut in eiusmod
-              amet ullamco enim sunt ex tempor reprehenderit cupidatat.
-            </p>
-            <p>
-              Adipisicing voluptate do et ullamco consequat enim anim id tempor
-              duis reprehenderit enim dolore irure. Labore aliqua nulla cillum
-              sit excepteur in. Ipsum cillum proident ipsum commodo duis ad qui.
-              Cupidatat veniam ipsum nisi proident incididunt. Voluptate tempor
-              ea eu incididunt proident laborum ad commodo labore duis cillum
-              eiusmod qui aliqua.
-            </p>
-            <p>
-              Et anim velit sint sit laboris irure nulla ex ullamco dolore.
-              Exercitation elit esse esse magna. Labore qui cillum quis sit non
-              quis voluptate nisi. Cillum nulla elit voluptate voluptate. Sint
-              sunt quis cupidatat ad excepteur eu Lorem do exercitation. Et
-              dolor sint labore amet culpa laboris amet cupidatat excepteur.
-            </p>
-          </UnitEntryContent>
+          <form onSubmit={this.onSubmit}>
+            <label>
+              Name
+            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </UnitEntryContainer>
       </ScrollComponent>
     );
@@ -211,6 +50,11 @@ export class ContactTile extends React.Component<
 
 export default ContactTile;
 
-export interface IContactTileProps {}
+export interface IContactTileProps { }
 
-interface IContactTileState {}
+interface IContactTileState {
+  mail?: string;
+  name?: string;
+  acceptsDSGVO?: boolean;
+  text?: string;
+}
