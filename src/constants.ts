d1b306a4dd3e4a869c322f1612647340261b8147
Thomas Thron <tt@intelligentgraphics.biz>
2018-11-16 13:26:52 +0100

    lighten up highlight color

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ export const Colors = {
 	DefaultTextColor: "#333333",
 	LightTextColor: "#666666",
 	LinkColor: "#000000",
-	LinkHoverColor: "#856234",
-	HighlightColor: "#856234",
-	ActiveMenuColor: "#888888",
+	LinkHoverColor: "#ab854f",
+	HighlightColor: "#ab854f",
+	ActiveMenuColor: "#8c6c40",
 	CaptionUnderlineColor: "#888888",
 	RulerColor: "#444444",
 	galleryArrayColor: "#cccccc"
