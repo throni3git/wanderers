eec2e4dcc40dc86eb7c76a79b7885b459873c1e3
Unknown <throni3git@gmail.com>
2020-08-04 02:03:34 +0200

    urlParameters: handle case-insensitive

--- src/urlParameters.ts
+++ src/urlParameters.ts
@@ -1,10 +1,10 @@
 // Url Parameter handling
-const url = new URL(window.location.href);
-export const debugCamera = url.searchParams.get("debugCamera") != null;
-export const debugOrbiting = url.searchParams.get("debugOrbiting") != null;
-export const debugContact = url.searchParams.get("debugContact") != null;
-export const hideSite = url.searchParams.get("hideSite") != null;
-export const startupTile = url.searchParams.get("startupTile") || "News";
+const url = new URL(window.location.href.toLowerCase());
+export const debugCamera = url.searchParams.get("debugcamera") != null;
+export const debugOrbiting = url.searchParams.get("debugorbiting") != null;
+export const debugContact = url.searchParams.get("debugcontact") != null;
+export const hideSite = url.searchParams.get("hidesite") != null;
+export const startupTile = url.searchParams.get("startuptile") || "News";
 export const darkTheme = url.searchParams.get("dark") != null;
 export const lightTheme = url.searchParams.get("light") != null;
 export const background2D = url.searchParams.get("2d") != null;
