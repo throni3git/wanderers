41573854b80a2b39768d0a72986750eef1428d2f
Unknown <throni3git@gmail.com>
2020-08-12 11:21:54 +0200

    fix babel setup

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,52 +1,65 @@
+import * as path from "path";
 import * as webpack from "webpack";
 import * as webpackDevServer from "webpack-dev-server";
-import * as copyPlugin from "copy-webpack-plugin";
-import * as cleanPlugin from "clean-webpack-plugin";
-import * as htmlPlugin from "html-webpack-plugin";
-import * as path from "path";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import copyWebpackPlugin from "copy-webpack-plugin";
 
 const isProduction = process.env.NODE_ENV === "production";
 const timestamp = JSON.stringify(new Date().toISOString());
 
 const config: webpack.Configuration = {
+	// webpack will take the files from ./src/index
 	entry: "./src/index.ts",
+
+	// adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
 	resolve: {
 		extensions: [".js", ".ts", ".tsx"]
 	},
+
 	module: {
 		rules: [
+			// we use babel-loader to load our jsx and tsx files
 			{
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        },
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
 			}
 		]
 	},
+
 	plugins: [
 		new webpack.DefinePlugin({
 			BUILD_TIMESTAMP: timestamp,
 			IS_PRODUCTION: isProduction
 		}),
-		new htmlPlugin({ title: "SOJUS3000", favicon: "favicon.png" }),
-		new cleanPlugin(isProduction ? ["dist/**/*"] : []),
-		new copyPlugin([
-			{ from: "src/mail_api_send.php", to: "", toType: "file" },
-			{ from: "assets/", to: "assets/" },
-			{ from: "data/", to: "data/" },
-			{ from: "media/", to: "media/" }
-		])
+		new HtmlWebpackPlugin({ title: "SOJUS3000", favicon: "favicon.png" }),
+		new CleanWebpackPlugin(),
+		new copyWebpackPlugin({
+			patterns: [
+				"src/mail_api_send.php",
+				{ from: "assets/", to: "assets/" },
+				{ from: "data/", to: "data/" },
+				{ from: "media/", to: "media/" }
+			]
+		})
 	],
+
+	// and output it into /dist as bundle.js with hash
 	output: {
 		path: path.resolve("dist"),
 		filename: "js/bundle.[chunkhash].js"
 	},
+
 	mode: isProduction ? "production" : "development",
+
 	devtool: isProduction ? false : "source-map",
+
 	devServer: {
 		port: 9000
 	} as webpackDevServer.Configuration
 };
 
-module.exports = config;
+export default config;
