73a1185b7956edec74e4721143720d656b42344b
Thomas Thron <throni3git@gmail.com>
2021-07-22 21:57:10 +0200

    update dependencies

--- src/content/tileComponents.tsx
+++ src/content/tileComponents.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Scrollbars } from "react-custom-scrollbars";
+import { Scrollbars } from "react-custom-scrollbars-2";
 
 import styled from "styled-components";
 
